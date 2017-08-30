65a5e9f61d92eaf080eb8d534e035d1f2178a4a5
mayijun <ma.yijun@outlook.com>
2017-08-30 20:10:26 +0800

    script to change password

--- start_notebook.sh
+++ start_notebook.sh
@@ -6,4 +6,15 @@ set -euo pipefail
 
 HASH=$(python -c "from notebook.auth import passwd; print(passwd('${PASSWORD:-admin}'))")
 
+echo "========================================================================"
+echo "You can now connect to this Ipython Notebook server using, for example:"
+echo ""
+echo "  docker run -d -p <your-port>:8888 -e password=<your-password> ipython/noetebook"
+echo ""
+echo "  use password: ${PASSWORD:-admin} to login"
+echo ""
+echo "========================================================================"
+
 unset PASSWORD
+
+jupyter lab --allow-root --NotebookApp.password="$HASH"
