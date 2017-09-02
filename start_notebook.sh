02555d7c4bd4b72a1c4246db523b3694a8d740c2
mayijun <ma.yijun@outlook.com>
2017-09-02 15:46:05 +0800

    add dashboard server config and enable dashboard bundler
    # referred jupyter-incubator/dashboards_setup

--- start_notebook.sh
+++ start_notebook.sh
@@ -7,12 +7,8 @@ set -euo pipefail
 HASH=$(python -c "from notebook.auth import passwd; print(passwd('${PASSWORD:-admin}'))")
 
 echo "========================================================================"
-echo "You can now connect to this Ipython Notebook server using, for example:"
-echo ""
-echo "  docker run -d -p <your-port>:8888 -e password=<your-password> ipython/noetebook"
-echo ""
+echo "You can now connect to this Ipython Notebook server "
 echo "  use password: ${PASSWORD:-admin} to login"
-echo ""
 echo "========================================================================"
 
 unset PASSWORD
